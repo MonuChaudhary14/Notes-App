fa6d41cfb8c340ce7a4b4c49e4a4c426736ced0a
Monu Chaudhary <monuchaudharypoonia@gmail.com>
2025-10-02 12:47:27 +0530

    DTO Added

---
+++ src/main/java/org/monu/notesapp/DTO/NotesDTO.java
@@ -0,0 +1,61 @@
+package org.monu.notesapp.DTO;
+
+public class NotesDTO {
+
+    private int id;
+    private String name;
+    private String title;
+    private String description;
+    private String emailId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "NotesDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", emailId='" + emailId + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/monu/notesapp/Mapper/NotesMapper.java
@@ -0,0 +1,23 @@
+package org.monu.notesapp.Mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.monu.notesapp.DTO.NotesDTO;
+import org.monu.notesapp.entity.NotesStructure;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface NotesMapper {
+
+    @Mapping(source = "notes", target = "description")
+    @Mapping(source = "email", target = "emailId")
+    NotesDTO notesToNotesDTO(NotesStructure notes);
+
+    List<NotesDTO> notesListToNotesDTOList(List<NotesStructure> notesList);
+
+    @Mapping(source = "description", target = "notes")
+    @Mapping(source = "emailId", target = "email")
+    NotesStructure notesDTOToNotesStructure(NotesDTO notes);
+
+}
